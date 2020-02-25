6f33a8188d7482ced282ab4f2fad3b682ef496ee
Maria Garcia <maria@example.com>
2020-02-25 15:04:34 +0000

    Initial commit

---
+++ R/create-plot.R
@@ -0,0 +1,19 @@
+# Simple script analysing the Australian weather data
+
+library("tidyverse")
+
+# Read in the data
+
+df = read_csv("data/weatherAUS.csv")
+
+# Create a scatter plot of Humidity9am and Temp9am
+# for the location "Brisbane"
+
+scatter_plot = 
+df %>%
+  filter(Location == "Brisbane") %>%
+  ggplot(aes(x = Temp9am, y = Humidity9am)) + 
+  geom_point()
+
+# Save the plot in graphics/
+ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
